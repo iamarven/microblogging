1394a88b02fd3d92d54b9585462a58de065bd54d
Maxim Shelofast <maximshelofast@gmail.com>
2025-04-24 17:23:40 +0300

    post service init

---
+++ post-service/src/main/java/com/merfonteen/postservice/entity/Post.java
@@ -0,0 +1,35 @@
+package com.merfonteen.postservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "posts_table", schema = "post_service")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author_id", nullable = false)
+    private Long authorId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "media_url")
+    private String mediaUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
