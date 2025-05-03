2ed6825503ecdd343e8549335d8dcdc694e52fd3
Maxim Shelofast <maximshelofast@gmail.com>
2025-05-03 17:01:05 +0300

    fix both dockerfile and docker-compose.yml

--- post-service/src/main/java/com/merfonteen/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/merfonteen/postservice/PostServiceApplication.java
@@ -1,5 +1,6 @@
 package com.merfonteen.postservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableCaching
 @SpringBootApplication
 public class PostServiceApplication {
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
     public static void main(String[] args) {
         SpringApplication.run(PostServiceApplication.class, args);
     }
