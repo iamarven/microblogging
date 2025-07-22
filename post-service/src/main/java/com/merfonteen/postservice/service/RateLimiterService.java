75a90f2f87d666437f64cb74cb058c50048d387b
Maxim Shelofast <maximshelofast@gmail.com>
2025-07-22 19:59:52 +0300

    test commit

--- post-service/src/main/java/com/merfonteen/postservice/service/RateLimiterService.java
+++ post-service/src/main/java/com/merfonteen/postservice/service/RateLimiterService.java
@@ -17,15 +17,6 @@ public class RateLimiterService {
     private static final Duration DURATION = Duration.ofMinutes(1);
 
     public void validatePostCreationLimit(Long userId) {
-        String key = "post_create:" + userId;
-        Long currentCount = stringRedisTemplate.opsForValue().increment(key);
 
-        if(currentCount != null && currentCount == 1) {
-            stringRedisTemplate.expire(key, DURATION);
-        }
-
-        if(currentCount != null && currentCount > POST_LIMIT) {
-            throw new TooManyRequestsException("You have exceeded the allowed number of posts per minute");
-        }
     }
 }
